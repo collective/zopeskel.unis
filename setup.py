5edc906bdb90edd266e00bf5c21831dad4c9d1a7
Encolpe DEGOUTE <encolpe.degoute@quadra-informatique.fr>
2011-04-20 03:39:55 +0200

    prepare the release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ This module contains the tool of zopeskel.unis
 import os
 from setuptools import setup, find_packages
 
-version = '1.6'
+version = '1.7'
 
 documentation = ''
 if os.path.exists(os.path.join("docs", "unis-zopeskel-usage.txt")):
