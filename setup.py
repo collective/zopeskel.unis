8d4b33334de1b129b85200bb3e622c8f85ddc39f
Encolpe DEGOUTE <encolpe.degoute@quadra-informatique.fr>
2012-05-04 12:08:27 +0200

    Fix collective.developermanual checkout using the readonly address for the git repository

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ This module contains the tool of zopeskel.unis
 import os
 from setuptools import setup, find_packages
 
-version = '1.11'
+version = '1.12'
 
 documentation = ''
 if os.path.exists(os.path.join("docs", "unis-zopeskel-usage.txt")):
